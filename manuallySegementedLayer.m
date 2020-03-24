3f39c3209314c41af2ac7ed6428e094dfbb451cd
Melli <philipp.matten@meduniwien.ac.at>
2020-03-24 11:49:45 +0100

    Added file for manually segementing a layer -> return mask with boarder pixels

---
+++ manuallySegementedLayer.m
@@ -0,0 +1,12 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                           Auxiliary function
+%                               copyright: 
+%       @melanie.wuest@zeiss.com & @philipp.matten@meduniwien.ac.at
+%
+%   Center for Medical Physics and Biomedical Engineering (Med Uni Vienna)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function [mask] = manuallySegementedLayer(image)
+
+
+end
