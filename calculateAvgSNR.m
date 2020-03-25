39c70b1f364bb6a8a6092420a48d3a73628212ee
Melli <philipp.matten@meduniwien.ac.at>
2020-03-25 11:34:23 +0100

    Added main loop and sub-function for bScan SNR cal

--- calculateAvgSNR.m
+++ calculateAvgSNR.m
@@ -7,6 +7,16 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 function [octSNR, octSNDDb] = calculateAvgSNR(octDataCube)
+    sz = size(octDataCube);
+    for i = 1:sz(3)
+        snr(i) = calcSliceSnr(octDataCube(:,:,i0));
+    end
 
+    octSNR = mean(snr);
+    octSNDDb = 20 * log10(octSNR);
+    
+end
 
+function [snrBScan] = calcSliceSnr(bScan)
+ 
 end
