577ed70ae10fd109f9faf91ddd0649776fc239bb
Melli <philipp.matten@meduniwien.ac.at>
2020-03-25 11:30:22 +0100

    Added function to calculate tomograms SNR on a per-bScan-basis

---
+++ calculateAvgSNR.m
@@ -0,0 +1,12 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                           Auxiliary function
+%                               copyright: 
+%       @melanie.wuest@zeiss.com & @philipp.matten@meduniwien.ac.at
+%
+%   Center for Medical Physics and Biomedical Engineering (Med Uni Vienna)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function [octSNR, octSNDDb] = calculateAvgSNR(octDataCube)
+
+
+end
