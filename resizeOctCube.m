a69baea2742eaa4aa402678e2d194bba7b6a149a
Melli <philipp.matten@meduniwien.ac.at>
2020-03-27 17:32:08 +0100

    Added and tested resizing functionality

--- resizeOctCube.m
+++ resizeOctCube.m
@@ -6,6 +6,16 @@
 %   Center for Medical Physics and Biomedical Engineering (Med Uni Vienna)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function [reSzCube] = resizeOctCube(image, fac)
+function [reSzCube] = resizeOctCube(cube, widthFac)
+
+sz = size(cube);
+reSzCube = zeros(sz(1), widthFac*sz(2), sz(3));
+
+for i = 1:sz(3)
+    bScan = cube(:,:,i);
+    reSzCube(:,:,i) = imresize(bScan, [sz(1), widthFac*sz(2)], 'bicubic');
+end
+
+reSzCube = uint8(reSzCube);
 
 end
