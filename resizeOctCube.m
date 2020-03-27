cffeea790e7e715adbfe417d116fd32d8ed4e817
Melli <philipp.matten@meduniwien.ac.at>
2020-03-27 17:14:28 +0100

    Added function to resize OCT cubes/ images

---
+++ resizeOctCube.m
@@ -0,0 +1,11 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                           Auxiliary function
+%                               copyright:
+%       @melanie.wuest@zeiss.com & @philipp.matten@meduniwien.ac.at
+%
+%   Center for Medical Physics and Biomedical Engineering (Med Uni Vienna)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function [reSzCube] = resizeOctCube(image, fac)
+
+end
