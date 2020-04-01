0165d40b5e5b287a427c0fe8d4f94d8fef1c595e
Philipp <philipp.matten@gmx.de>
2020-04-01 14:11:44 +0200

    Added data hyper handling and both offsets a parameters

--- segmentImage.m
+++ segmentImage.m
@@ -1,12 +1,13 @@
-function [seg, mns] = segmentImage(input, mask, v)
-
-    adjMat = createAdjMat(input, mask, v, 1e-7);
+function [seg, mns] = segmentImage(input, mask, offset1, offset2)
+    
+    image = createGradImg(single(input));
+    adjMat = createAdjMat(image, mask, offset1, offset2);
     grph = digraph(adjMat);
     path = shortestpath(grph, 1, size(adjMat, 1));
-    [pathX, pathY] = ind2sub(size(input), path);
+    [pathX, pathY] = ind2sub(size(image), path);
     seg = pathX(gradient(pathY) ~= 0);
     
-    sz = size(input);
+    sz = size(image);
     if numel(seg) > sz(2)
         seg = seg(1:sz(2));
     end
