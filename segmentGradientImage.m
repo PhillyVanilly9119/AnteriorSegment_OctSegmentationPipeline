57ab9617e684dd038743ad82aacf7bde6957554a
Philipp <philipp.matten@gmx.de>
2020-04-02 23:22:46 +0200

    Ideas for logic to find corneal endothelium

--- segmentGradientImage.m
+++ segmentGradientImage.m
@@ -9,14 +9,27 @@
 function [segmImg] = segmentGradientImage(image)
 
 sz = size(image);
+image = single(image);
 segmImg = zeros(sz(1),sz(2));
 
 gradImg = createGradImg(image);
 
 for i = 1:sz(2)
-    pts(:,i) = maxk(gradImg(:,i), 3); %3 because of the 3 expected layers
+    [val, col] = maxk(gradImg(:,i), 1); %3 because of the 3 expected layers
+    segmImg(col, i) = val;
     %    mPts(:,i) = mink(gradImg(:,i), 3); % also 3 because I dont yet understand the gradient
 end
 
+%% think of logic for a loop to subsequently find max values on endothel
+% -> find logic start/ boundary condition at image margins
+% A = rand([15 129])*100; %matrix with random values
+% [~,I] = min(abs(A - 15),[],1); %get indices for values closest to 15
+% logicalMat = false(size(A)); %preallocate logical matrix
+% arrayLength = size(A,2);
+% for jj = 1:arrayLength
+%     logicalMat(I(jj),jj) = 1; %fill logical matrix
+% end
+
+imagesc(segmImg);
 
 end
