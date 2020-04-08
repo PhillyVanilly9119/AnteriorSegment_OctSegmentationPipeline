892cda9fd120257b031767c281229f8827b37a60
Philipp <philipp.matten@gmx.de>
2020-04-08 10:30:01 +0200

    Added segmentation algo for both layers on basis of a filtered aScans (moving average filter)

--- PlayingAround.m
+++ PlayingAround.m
@@ -41,3 +41,100 @@ imshow(f_img);
 % smoothAScans = smoothenAScans(bScan, 3, 25); % Params: image, Order, Length
 % figure; plot(bScan(:,512)); hold on; plot(smoothAScans(:,512));
 % figure; imshow(smoothAScans)
+
+
+%% Segmentation
+%____________________________________________________
+% %% Try by finding mean (slope) of upcoming 5 values
+% qtr = round(sz(1)/4);
+% hlf = round(sz(1)/2);
+% n = 3; %number if mins and maxes per row
+% for i = 1:length(hlf-qtr:hlf+qtr)-1 %-4 cause shortest nect 3 values are averaged
+%     [~, posMax] = maxk(gradImg(:,i+qtr), n);
+%     if i > 1
+%         for ii = 1:n
+%             dists(ii) = abs(meanNextMaxCurve(i-1) - posMax(ii));
+%         end
+%         [~, shortestNN] = min(dists);
+%         meanNextMaxCurve(i) = posMax(shortestNN);
+%     else
+%         meanNextMaxCurve(i) = round(mean(posMax(:,i)));
+%     end
+% end
+%
+% figure;
+% imagesc(gradImg);
+% colormap gray;
+% hold on, plot(hlf-qtr:(hlf+qtr-1), meanNextMaxCurve);
+%
+% p = polyfit(1:sz(1), meanNextMaxCurve, 2);
+% fitMeanNextMaxCurve = polyval(p, 1:sz(1));
+%
+% figure;
+% imagesc(gradImg);
+% colormap gray;
+% hold on, plot(curve);
+% im = gradImg(1:300,256:3*256);
+% sz = size(im);
+% imshow(im)
+%
+%
+% BW1 = edge(image, 'Sobel', 'vertical');
+% BW2 = edge(image, 'log', 0.0001);
+% imshowpair(BW1,BW2,'montage')
+% colormap gray;
+
+%_______________________________________________________
+% %% Find curve based on differeces if extrema (per row)
+% % mean(max(col)-max(col))
+
+% qtr = round(sz(1)/4);
+% hlf = round(sz(1)/2);
+% n = 4; %number if mins and maxes per row
+% for i = 1:length(hlf-qtr:hlf+qtr)-1
+%     [~, posMax] = maxk(gradImg(:,i+qtr), n);
+%     [~, posMin] = mink(gradImg(:,i+qtr), n);
+%     maxMinDiffs = zeros(n, n);
+%     for ii = 1:length(posMax)
+%         maxMinDiffs(ii,:) = abs(posMax(ii) - posMin);
+%     end
+%     [r,c] = find(maxMinDiffs == min(maxMinDiffs(:)));
+%     extDiffCurve(i) = abs(mean(posMax(r(1)), posMin(c(1))));
+% end
+% % fit polynominal
+% pCoeffs = polyfit(hlf-qtr:(hlf+qtr-1), extDiffCurve, 2);
+% fitExtDifCurve = polyval(pCoeffs, 1:sz(1));
+% %plot
+% figure; imagesc(gradImg);
+% colormap gray;
+% hold on, plot(1:sz(1), fitExtDifCurve);
+% plot(hlf-qtr:(hlf+qtr-1), extDiffCurve)
+
+%_____________________________________________________
+% %%Try by finding the most consecutive values == 0.5 (
+% boarders are that after resizing)
+%
+% for i = 1:sz(1)
+%     deltas = diff(gradImg(:,i));
+%     [r,~] = find(deltas == 0);
+%     counter = 1;
+%     for ii = 1:(length(r)-1)
+%         if r(ii)+1 == r(ii+1)
+%             counter = counter + 1;
+%             tracking(ii) = counter;
+%         else
+%             counter = 1;
+%             tracking(ii) = counter;
+%         end
+%         [~,pos] = max(tracking);
+%         curve(i) = r(pos);
+%     end
+%     clear tracking
+% end
+%
+% filtered = sgolayfilt(curve(100:200), 2, length(100:200));
+% % filtered = sgolayfilt(filtered, 2, sz(1)-1);
+% figure;
+% imagesc(gradImg);
+% colormap gray;
+% hold on, plot(100:200, filtered);
--- segmentGradientImage.m
+++ segmentGradientImage.m
@@ -3,33 +3,84 @@
 %                               copyright:
 %       @melanie.wuest@zeiss.com & @philipp.matten@meduniwien.ac.at
 %
+%                         Note: LOI* = Layer of interest
+%
 %   Center for Medical Physics and Biomedical Engineering (Med Uni Vienna)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function [segmImg] = segmentGradientImage(image)
-
+function [segmImg] = segmentGradientImage(image, label)
+%% Preprocessing
+% globals
 sz = size(image);
-image = single(image);
+curve = zeros(sz(1), 2);
 segmImg = zeros(sz(1),sz(2));
-
-gradImg = createGradImg(image);
-
-for i = 1:sz(2)
-    [val, col] = maxk(gradImg(:,i), 1); %3 because of the 3 expected layers
-    segmImg(col, i) = val;
-    %    mPts(:,i) = mink(gradImg(:,i), 3); % also 3 because I dont yet understand the gradient
+gradImg = createGradImg(single(image));
+%Determine neccessary segmentation steps
+if label == 1 % case ONLY ENDOTHELIUM
+    nExtrema = 2; %assuming ENDOthlium and EPIThelium
+    %     pos = 1;
+elseif label == 2 % case BOTH
+    nExtrema = 3; %assuming all 3 layers
+    %     pos = [1,2];
+else
+    disp("Neither layer selection was passed through the label parameter...");
+    return;
 end
 
-%% think of logic for a loop to subsequently find max values on endothel
-% -> find logic start/ boundary condition at image margins
-% A = rand([15 129])*100; %matrix with random values
-% [~,I] = min(abs(A - 15),[],1); %get indices for values closest to 15
-% logicalMat = false(size(A)); %preallocate logical matrix
-% arrayLength = size(A,2);
-% for jj = 1:arrayLength
-%     logicalMat(I(jj),jj) = 1; %fill logical matrix
-% end
+%% Filter bScan on aScan-basis
+% TODO: Try alternatively with derivative of a-Scan
+a = 1;
+windowSize = 15;
+b = (1/windowSize)*ones(1,windowSize);
+filteredAScans = zeros(sz(1), sz(2));
+for i = 1:sz(1)
+    filteredAScans(:,i) = filter(b,a,gradImg(:,i));
+end
 
-imagesc(segmImg);
+%% Find extrema (i.e. bScans' boaders)
+% 1): Endothelium
+qtr = round(sz(1)/4);
+hlf = round(sz(1)/2);
+endoVec = hlf-qtr:(hlf+qtr-1);
+for i = 1:length(endoVec)
+    [~, cMax] = maxk(filteredAScans(:,i+qtr), nExtrema);
+    [~, cMin] = mink(filteredAScans(:,i+qtr), nExtrema);
+    if label == 1 %if only ENDO is visible
+        %Endo-layer is last max-layer
+        curve(i+qtr,1) = round(mean(cMax(end), cMin(end)));
+    elseif label == 2 %if both OVD and ENDO are visible
+        %Endo-layer is second to last max-layer
+        curve(i+qtr,1) = round(mean(cMax(end-1), cMin(end-1)));
+    else %of no LOI* is visible
+        curve(i+qtr,1) = 0;
+    end
+end
+
+% 2): OVD
+for i = 1:sz(1)
+    [~, cMax] = maxk(filteredAScans(:,i), nExtrema);
+    [~, cMin] = mink(filteredAScans(:,i), nExtrema);
+    if label == 2
+        curve(i,2) = round(mean(cMax(end), cMin(end)));
+    end
+end
+
+%% Fit the two curves
+%Endothel -> if == 0-vector, retuns still a 0-vector
+endoPolCoeffs = polyfit(endoVec, curve(endoVec,1)', 2);
+fittedEndothel = polyval(endoPolCoeffs, 1:sz(1));
+curve(:,1) = round(fittedEndothel);
+curve(:,2) = round(curve(:,2));
+
+%% Write boarders into Mask
+%TODO: Write (all available i.e non-0 curves) into segemented mask (segmImg)
+for i = 1:sz(1)
+    if curve(i,1) ~= 0
+        segmImg(curve(i,1),i) = 1;
+    end
+    if curve(i,2) ~= 0
+        segmImg(curve(i,2),i) = 1;
+    end
+end
 
 end
