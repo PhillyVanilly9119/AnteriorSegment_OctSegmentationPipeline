9e4ecb4576edf53cf378af25b347a944daefe86c
Philipp <philipp.matten@gmx.de>
2020-04-08 21:42:37 +0200

    Added saving of segemented layers on bScan

--- saveCalculatedMask.m
+++ saveCalculatedMask.m
@@ -6,18 +6,21 @@
 %   Center for Medical Physics and Biomedical Engineering (Med Uni Vienna)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function [] = saveCalculatedMask(mask, maskFolder, bScanIDX)
-    
-    %save bin-file
-    binID = sprintf('mask_of_bScanNo%0.0f.bin', bScanIDX);
-    fileID = fopen(fullfile(maskFolder, binID), 'w');
-    fwrite(fileID, uint8(mask));
-    fclose(fileID);
-    
-    %save images
-    imMask1 = sprintf('mask_of_bScanNo%0.0f_Cornea.png', bScanIDX);
-    imMask2 = sprintf('mask_of_bScanNo%0.0f_OVA.png', bScanIDX);
-    imwrite(mask(:,:,1),fullfile(maskFolder, imMask1))
-    imwrite(mask(:,:,2),fullfile(maskFolder, imMask2))
+function [] = saveCalculatedMask(curve, mask, image, maskFolder, bScanIDX)
+
+%save mask to bin-file
+binID = sprintf('mask_of_bScanNo%0.0f.bin', bScanIDX);
+fileID = fopen(fullfile(maskFolder, binID), 'w');
+fwrite(fileID, uint8(mask));
+fclose(fileID);
+
+%save overlayed images of bScan
+f = figure('visible', 'off');
+imagesc(image); 
+colormap gray; 
+hold on, plot(curve); 
+imMask1 = sprintf('mask_of_bScanNo%0.0f.png', bScanIDX);
+saveas(f, fullfile(maskFolder, imMask1));
+close(f)
 
 end
