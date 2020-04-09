323b08e095d60e7fe289683297757c0fe3ef5c5c
Philipp <philipp.matten@gmx.de>
2020-04-09 13:05:19 +0200

    Added more robust interpolation for selected OVD points

---
+++ interpolateBetweenSegmentedPoints.m
@@ -0,0 +1,31 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                           Auxiliary function
+%                               copyright:
+%       @melanie.wuest@zeiss.com & @philipp.matten@meduniwien.ac.at
+%
+%   Center for Medical Physics and Biomedical Engineering (Med Uni Vienna)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function [curve] = interpolateBetweenSegmentedPoints(pts, imgWitdh, imgHeight)
+
+% pts = (2 x N) points
+% where first row is pos. in imgWidth and second row is pos. in imgHeight
+pts = round(pts);
+
+heightPts = interp1(pts(1,:), pts(2,:), min(pts(1,:)):max(pts(1,:)), 'cubic');
+
+%Add for loop to fill non-interpolated values with 0s, which will belater
+%denote spots were no layer is drawn
+curve = zeros(imgWitdh, 1);
+for i = 1:imgWitdh
+    if all(i >= min(pts(1,:)) & i <= max(pts(1,:)))
+        curve(i) = round(heightPts(i-(min(pts(1,:)))+1));
+    else
+        curve(i) = 0;
+    end
+end
+
+curve(curve(:) > imgHeight) = imgHeight;
+curve(curve < 0) = 0;
+
+end
