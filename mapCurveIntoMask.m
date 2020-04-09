55b07d3a407210346d3d6865f3ca391b2ef3c26a
Philipp <philipp.matten@gmx.de>
2020-04-09 13:40:30 +0200

    Added mapping of manually segmented curves in extra function

---
+++ mapCurveIntoMask.m
@@ -0,0 +1,23 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                           Auxiliary function
+%                               copyright:
+%       @melanie.wuest@zeiss.com & @philipp.matten@meduniwien.ac.at
+%
+%   Center for Medical Physics and Biomedical Engineering (Med Uni Vienna)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function [mask] = mapCurveIntoMask(DataStruct, curve)
+
+mask = zeros(DataStruct.processingVolumeDims(1),...
+    DataStruct.processingVolumeDims(2));
+
+for i = 1:DataStruct.processingVolumeDims(1)
+    if curve(i,1) ~= 0
+        mask(curve(i,1),i) = 1;
+    end
+    if curve(i,2) ~= 0
+        mask(curve(i,2),i) = 1;
+    end
+end
+
+end
