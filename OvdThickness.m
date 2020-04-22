3880508ccce3cc2eead78c249a015d6027460301
Melli <philipp.matten@meduniwien.ac.at>
2020-04-22 15:24:44 +0200

    Changes Color map and changed logic for thickness calc on b-Scan basis

--- OvdThickness.m
+++ OvdThickness.m
@@ -1,3 +1,11 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                           Auxiliary function
+%                               copyright:
+%       @melanie.wuest@zeiss.com & @philipp.matten@meduniwien.ac.at
+%
+%   Center for Medical Physics and Biomedical Engineering (Med Uni Vienna)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 %loading of the masks
 dir = uigetdir();
 maskVolume = loadMasksFromFile(dir, 1024, 1024, 'png');
@@ -7,24 +15,33 @@ maskVolume(maskVolume==255)=1;
 maskSZ = size(maskVolume);
 
 % zero matrix (128, 1024)
-thicknessMap = zeros(maskSZ(3), maskSZ(2)); 
+thicknessMap = zeros(maskSZ(3), maskSZ(2));
 
 for i = 1:maskSZ(3)
-   onemask = maskVolume(:,:,i);
-   thicknessMap(i,:) = calculateThicknessPerBscan(onemask);
-end 
+    onemask = maskVolume(:,:,i);
+    thicknessMap(i,:) = calculateThicknessPerBscan(onemask);
+end
 
-thicknessMap = imresize(thicknessMap, [128, 512]);
+thicknessMap = round(abs(imresize(thicknessMap, [128, 512], 'bicubic')));
+%%TODO: think about if rounding to interger multiples of µm is valid in
+%%this case...
+% thicknessMap = abs(imresize(thicknessMap, [128, 512], 'bicubic'));
 
 Median_OvdThickness = median(nonzeros(thicknessMap))
-Mean_OvdThickness = mean(nonzeros(thicknessMap)')
+Mean_OvdThickness = mean(nonzeros(thicknessMap))
+NumberofValues_OvdThickness = numel(nonzeros(thicknessMap))
 
 figure
+%Change color map so that invalid points in thickness map are changes to a
+%grey shade
+cmap = hot(256);
+cmap(1,:) = [0.5;0.5;0.5]; % grey
+colormap(cmap); % activate it
 imagesc(thicknessMap)
-colorbar
+
 axis square
 title('2D Falschfarbenprojektion der OVD-Schicht in µm (Z-HYALCOAT)')
 ylabel('BScans')
 
-figure
-surf(thicknessMap)
+% figure
+% surf(thicknessMap)
--- calculateThicknessPerBscan.m
+++ calculateThicknessPerBscan.m
@@ -1,19 +1,32 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                           Auxiliary function
+%                               copyright:
+%       @melanie.wuest@zeiss.com & @philipp.matten@meduniwien.ac.at
+%
+%   Center for Medical Physics and Biomedical Engineering (Med Uni Vienna)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 function [thickness] = calculateThicknessPerBscan(onemask)
-one_px_in_micron=2900/1024;
-n_ovd=1.333412;
 
+one_px_in_micron = 2900/1024;
+n_ovd = 1.333412;
+scale = 1.34 * one_px_in_micron;
 sz = size(onemask);
-% sz = size(mask); %[1024, 1024] = [länge, breite]  
-thickness = zeros(1,sz(2)); 
+% sz = size(mask); %[1024, 1024] = [länge, breite]
+thickness = zeros(1,sz(2));
 
-    for i = 1:sz(2)     
-         if nnz(onemask(:,i))==2 
-            thickness(1,i) = (1.34*one_px_in_micron*(max(find(onemask(:,i)==1))-min(find(onemask(:,i)==1)))/n_ovd);
-              else
-                  thickness(1,i) = 0;
-         end
+for i = 1:sz(2)
+%     if ~unique(onemask(:,i)==1)
+    if nnz(onemask(:,i)) == 2
+        thickness(1,i) = scale * ( (find(onemask(:,i), 1, 'last')) - find(onemask(:,i), 1, 'first') ) / n_ovd ;
+        if thickness(1,i) == 0
+            thickness(1,i) = 1;
+        end
+    else
+        thickness(1,i) = 0;
     end
-  
- ans = thickness;
+end
+
+
 end
 
