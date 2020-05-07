7752a28538efca13c9360f28ce58b6b235d762fb
Philipp <philipp.matten@gmx.de>
2020-05-07 18:12:20 +0200

    Created function to load bScans while deleting unwanted files (created from RESCNA))

---
+++ loadBScanImages.m
@@ -0,0 +1,33 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                           Auxiliary function
+%                               copyright: 
+%       @melanie.wuest@zeiss.com & @philipp.matten@meduniwien.ac.at
+%
+%   Center for Medical Physics and Biomedical Engineering (Med Uni Vienna)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function [octCube] = loadBScanImages(path, aScanLength, bScanLength, imgDtTypr)
+
+cd(path)
+files = dir(strcat('*.', imgDtTypr));
+delete *.xml *.jpg 
+if exist('enface.bmp', 'file')
+    delte enface.bmp
+end
+octCube = [];
+
+for i = 1:length(files)
+    if isfile(fullfile(path, files(i).name))
+        tmp = imread(files(i).name);
+        if length(tmp(:,1)) == aScanLength && length(tmp(1,:)) == bScanLength
+            octCube(:,:,i) = tmp;
+        else
+            fprintf("Image NO.%0.0f has a different size than was expected!\n", i);
+        end
+    else
+        disp("Input path does not contain a or the right file!");
+    end
+    octCube = uint8(octCube);
+end
+
+end
