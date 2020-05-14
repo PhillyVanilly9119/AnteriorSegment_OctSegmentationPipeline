795582d4082d7fd156be4c0698ae2eb66f34a6f8
Philipp <philipp.matten@gmx.de>
2020-05-14 20:35:04 +0200

    Function to create binary mask containing the cornea and OVD area as ones

---
+++ createBinaryMask.m
@@ -0,0 +1,13 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                           Auxiliary function
+%                               copyright:
+%       @melanie.wuest@zeiss.com & @philipp.matten@meduniwien.ac.at
+%
+%   Center for Medical Physics and Biomedical Engineering (Med Uni Vienna)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function [interpolatedMask] = createBinaryMask(DataStruct, curve)
+
+
+
+end
