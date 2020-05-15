2d05d7c63c0a7047a86e3a158cc0a8188d8041b0
Melli <philipp.matten@meduniwien.ac.at>
2020-05-15 13:44:36 +0200

    Maybe check out again...

--- ThicknessEvaluation/OvdThickness.m
+++ ThicknessEvaluation/OvdThickness.m
@@ -6,6 +6,9 @@
 %   Center for Medical Physics and Biomedical Engineering (Med Uni Vienna)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+%%% type OVD name
+OVD_name= 'Z-HYALCAOT';
+
 %loading of the masks
 dir = uigetdir();
 maskVolume = loadMasksFromFile(dir, 1024, 1024, 'png');
@@ -27,24 +30,31 @@ thicknessMap = round(abs(imresize(thicknessMap, [128, 512], 'bicubic')));
 %%this case...
 % thicknessMap = abs(imresize(thicknessMap, [128, 512], 'bicubic'));
 
-% 2-D Plot of OVD-Thickness
+%% 2-D Plot of OVD-Thickness
 figure
 %Change color map so that invalid points in thickness map are changes to a
 %grey shade
 cmap = hot(256);
-cmap(1,:) = [0.5;0.5;0.5]; % grey
+cmap(1,:) = [1;1;1]; % grey
 colormap(cmap); % activate it
 imagesc(thicknessMap)
 colorbar
 axis square
-title('OCT OVD thickness map in µm (Z-HYALCOAT)')
+title(['OVD thickness map in µm (' OVD_name ')'])
 ylabel('BScans')
 xlabel('AScans')
 
-% 3-D Plot of OVD-Thickness
+
+%% 3-D Plot of OVD-Thickness
 figure
 cmap = hot(256);
-cmap(1,:) = [0.5;0.5;0.5]; % grey
+cmap(1,:) = [0, 0.9, 0.1]; % grey
+
+% threshold = 200;
+% cmap = hot(256, lower=threshold, upper = max)
+% 
+% displayMap = thicknessMap(thicknessMap < threshold)
+
 colormap(cmap);
 y = 1:64;
 x = 1:64;
@@ -53,25 +63,61 @@ F = imresize(thicknessMap, [64, 64], 'bicubic');
 surf(X,Y,F)
 colorbar
 
+%% Histogramm with all valid values
 % Count numbers within binranges
 figure
 ThicknessValueswithoutZero=nonzeros(thicknessMap);
 threshold_thickness = 200;
-treshold_mask = ThicknessValueswithoutZero > 200;
+threshold_mask = ThicknessValueswithoutZero > 200;
 maxbin = max(thicknessMap, [], 'all');
 bin_edges = 0:50:maxbin;
-histogram(ThicknessValueswithoutZero(treshold_mask),bin_edges, 'FaceColor', 'red');
-hold on
-histogram(ThicknessValueswithoutZero(~treshold_mask),bin_edges, 'FaceColor', 'black');
-hold off
-title('Histogram of OVD thickness')
+
+%absolute frequency:
+histogram(ThicknessValueswithoutZero(threshold_mask),bin_edges, 'FaceColor', 'red'); hold on
+histogram(ThicknessValueswithoutZero(~threshold_mask),bin_edges, 'FaceColor', 'black'); hold off
+
+title(['Histogram of OVD thickness (' OVD_name ')'])
 xlabel('OVD-Thickness in µm')
 ylabel('Absolute frequency')
-% relative frequency:
-% histogram(ThicknessValueswithoutZero(treshold_mask),bin_edges, 'Normalization', 'probability')
+ylim([0 6000])
 
+%% calculate percentage above threshold (all valid values)
 binranges=[0, threshold_thickness , maxbin];
 [bincounts,ind] = histc(nonzeros(thicknessMap),binranges);
 percentage_thickness_greaterthan = bincounts(2,1)/numel(nonzeros(thicknessMap))
 
+% plot calculate percantage in histogramm 
+NE = [max(xlim) max(ylim)]-[diff(xlim) diff(ylim)]*0.05;
+text(NE(1), NE(2), [num2str(percentage_thickness_greaterthan*100) '%'],  'VerticalAlignment','top', 'HorizontalAlignment','right', 'color', 'red')
 
+% relative frequency:
+% histogram(ThicknessValueswithoutZero(threshold_mask),bin_edges, 'Normalization', 'probability')
+
+
+
+%% Histogramm with all valid values in centre area (3 mm diameter)
+% Count numbers within binranges
+figure
+ThicknessValueswithoutZero=nonzeros(thicknessMap(43:86, 171:342));
+threshold_thickness = 200;
+threshold_mask = ThicknessValueswithoutZero > 200;
+maxbin = max(thicknessMap(43:86, 171:342), [], 'all');
+bin_edges = 0:50:maxbin;
+
+%absolute frequency:
+histogram(ThicknessValueswithoutZero(threshold_mask),bin_edges, 'FaceColor', 'red'); hold on
+histogram(ThicknessValueswithoutZero(~threshold_mask),bin_edges, 'FaceColor', 'black'); hold off
+
+title('Histogram of OVD thickness in centre area diameter 3 mm (DisCoVisc)')
+xlabel('OVD-Thickness in µm')
+ylabel('absolute frequency')
+
+
+%% calculate percentage above threshold (all valid values within 3 mm centre area)
+binranges=[0, threshold_thickness , maxbin];
+[bincounts,ind] = histc(nonzeros(thicknessMap(43:86, 171:342)),binranges);
+percentage_thickness_greaterthan_centre_area = bincounts(2,1)/numel(nonzeros(thicknessMap(43:86, 171:342)))
+
+% plot calculate percantage in histogramm (centre area)
+NE = [max(xlim) max(ylim)]-[diff(xlim) diff(ylim)]*0.05;
+text(NE(1), NE(2), [num2str(percentage_thickness_greaterthan_centre_area*100) '%'],  'VerticalAlignment','top', 'HorizontalAlignment','right', 'color', 'red')
--- ThicknessEvaluation/calculateThicknessPerBscan.m
+++ ThicknessEvaluation/calculateThicknessPerBscan.m
@@ -13,18 +13,19 @@ n_ovd = 1.333412;
 scale = 1.34 * one_px_in_micron;
 sz = size(onemask);
 % sz = size(mask); %[1024, 1024] = [länge, breite]
-thickness = zeros(1,sz(2));
+thickness = NaN(1,sz(2));
+% thickness = zeros(1,sz(2));
 
 for i = 1:sz(2)
 %     if ~unique(onemask(:,i)==1)
     if nnz(onemask(:,i)) == 2
         thickness(1,i) = scale * ( (find(onemask(:,i), 1, 'last')) - find(onemask(:,i), 1, 'first') ) / n_ovd ;
-        if thickness(1,i) == 0
-            thickness(1,i) = 1;
-        end
-    else
-        thickness(1,i) = 0;
-    end
+%         if thickness(1,i) == 0
+%             thickness(1,i) = 1;
+%         end
+%     else
+%         thickness(1,i) = NaN;
+%     end
 end
 
 
