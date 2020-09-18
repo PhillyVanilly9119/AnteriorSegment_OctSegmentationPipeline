d8d97687f0a95da744c21f3bcb5e72fe1a6b5cce
Philipp <philipp.matten@gmx.de>
2020-09-18 10:49:53 +0200

    Added run file for adding data to existing data base

---
+++ MachnineLearningSegmentation/AddTrainingDataToExistingSet.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 18 10:48:05 2020
+
+@author: phili
+"""
+
+import BackendFunctions as Backend
+
+if __name__ == '__main__' :
+    Backend.add_data_to_main_folder()
+     
