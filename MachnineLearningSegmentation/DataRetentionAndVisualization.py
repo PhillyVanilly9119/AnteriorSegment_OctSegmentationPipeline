dbfbe3778e239d1b4adebb456d0f66c03f0a1c97
Philipp <philipp.matten@gmx.de>
2021-02-10 16:23:41 +0100

    Started implementing optical index conversion for heat maps

--- MachnineLearningSegmentation/DataRetentionAndVisualization.py
+++ MachnineLearningSegmentation/DataRetentionAndVisualization.py
@@ -40,16 +40,34 @@ if __name__ == '__main__' :
     pre_string = 'CorrectedAndInterpolatedThicknessmap_'
     file_dtype = '.mat'
     
+    index_dict = {
+        "provisc": 1.357,
+        "zhyalinplus": 1.364,
+        "amviscplus": 1.356,
+        "discovisc": 1.337,
+        "healonendocoat": 1.357,
+        "viscoat": 1.356,
+        "zhyalcoat": 1.343,
+        "combivisc": 1.353,
+        "duovisc": 1.356,
+        "twinvisc": 1.353,
+    }
+        
     for i, files in enumerate(sub_dirs) :
         current_file = os.path.split(files)
         current_file_name = current_file[-1]
-        file_name_parts = current_file_name.split('_')[0] 
-        print(file_name_parts)
-        break
+        file_name_parts = current_file_name.split('_')[1] 
+        for name, index in index_dict.items() :
+            if file_name_parts.lower == name : 
+                ovd_index = index   
+            else :
+                raise ValueError("Could not find name of OVD in file name!") 
         current_file_path = os.path.join(current_file[0], current_file_name, (pre_string + current_file_name + file_dtype))
         if os.path.isfile(current_file_path) :
             current_heat_map = np.asarray(scipy.io.loadmat(current_file_path).get('INTERPOL_THICKNESS_MAP'))
             # Process the thicknesses with the optical index and then save them
+            conversion_factor = 2900 / 512 * 1.34 / ovd_index # convert from pixels to microns
+            current_heat_map_um = np.asarray(conversion_factor * current_heat_map)
             
-            scipy.io.savemat(os.path.join(new_saving_dir, ('InterpolatedThicknessmap_' + current_file_name + '.mat')),  
-                            {'INTERPOL_THICKNESS_MAP': current_heat_map.astype(np.uint16)}) 
+            # scipy.io.savemat(os.path.join(new_saving_dir, ('InterpolatedThicknessmap_' + current_file_name + '.mat')),  
+            #                 {'INTERPOL_THICKNESS_MAP': current_heat_map_um.astype(np.uint16)})
