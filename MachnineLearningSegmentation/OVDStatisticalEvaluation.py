08c77a7f877e29dadb315e13ed2ad4039358b792
Philipp <philipp.matten@gmx.de>
2021-03-08 12:10:36 +0100

    Added script to statistically evaluate the thickness maps (ranksum)

---
+++ MachnineLearningSegmentation/OVDStatisticalEvaluation.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 22 13:32:10 2020
+
+@author:    Philipp
+            philipp.matten@meduniwien.ac.at
+
+@copyright: Medical University of Vienna,
+            Center for Medical Physics and Biomedical Engineering
+                
+"""
+
+import os
+import numpy as np
+from scipy.stats import wilcoxon, ranksums, RanksumsResult   
+import matplotlib.pyplot as plt
+
+def apply_ranksumtest(set1, set2, text) :
+    ranksums
+    pass
+
+def apply_wilcoxon(set1, set2, text) :
+    ranksums
+    pass
+
+def get_ranksum_result(set1, set2, text) :
+    ranksums
+    pass
+
+if __name__ == '__main__' :
+    # path for loading
+    path_loading = r''
+    # load data
+    
+    # apply test
+    apply_ranksumtest()
+
